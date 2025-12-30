b633a02718e0e79f7349f15173908ad8184fadee
Pooja Mishra <pooja.mishra5@wipro.com>
2025-12-30 13:34:57 +0530

    Food Project

--- app.js
+++ app.js
@@ -1,26 +1,78 @@
- // core react element , We have to create html element insdie the react using createElement
- 
-/* {} - where you will give attributes to your tags okay this object is the place where you will give
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import "bootstrap/dist/css/bootstrap.css";
 
-attributes to your tags what is the attribute to a tag suppose if my h1 if my h1 tag needs to have */
-/* <div id="parent">
-   <div id="child">
-      <h1></h1>
-           <h2></h2>
-   </div>
-   <div id="child">
-      <h1></h1>
-           <h2></h2>
-   </div>
-</div> */
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img
+          className="logo"
+          src="https://img.freepik.com/free-vector/burger-mascot-logo-design_779267-1066.jpg?semt=ais_hybrid&w=740&q=80"
+        ></img>
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+// const RestroCard = (resName, cuisine) => its calling destrucring
+const RestroCard = (props) => {
+  const { resData } = props;
+  return (
+    <div className="res-card" style={{ backgroundColor: "#f0f0f0" }}>
+      <img
+        className="res-logo"
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS2zCPmUwWrRfNmz9_UnZL3KoW3bNMHtTr9Qg&s"
+      />
+      <h3>{resData.resName}</h3>
+      <h5>{resData.cuisine}</h5>
+      <h5>4.4 star</h5>
+      <h5>38mintes </h5>
+    </div>
+  );
+};
 
-// parent is the object not a element tag 
-const parent = React.createElement("div" , {id:"parent"} , [React.createElement("div" ,{id:"child"} ,[React.createElement("h1",{},"I am h1 tag") , React.createElement("h2",{},"I am h1 tag")] ) , React.createElement("div" ,{id:"child"} ,[React.createElement("h1",{},"I am h1 tag") , React.createElement("h2",{},"I am h1 tag")] )] )
+const resObject = [
+  {
+    id: 1,
+    resName: "Meghana Restaurant",
+    cuisine: "North Indian,South,North",
+  },
+  {
+    id: 2,
+    resName: "KFC",
+    cuisine: "Burger,Fast Food",
+  },
+];
 
-    //const heading = React.createElement("h1",{id:"heading" , xyz: "abc"},"Hello World From React");
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search">Search</div>
+      <div className="res-container">
+        {resObject.map((res) => (
+          <RestroCard key={res.id} resData={res} />
+        ))}
+      </div>
+    </div>
+  );
+};
 
-    // but for randering the html element we nee dom or root dom so we will used createRoot but using dom 
-
-   const root =  ReactDOM.createRoot(document.getElementById("root"));
-
-   root.render(parent);
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+      <Body />
+    </div>
+  );
+};
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<AppLayout />);
